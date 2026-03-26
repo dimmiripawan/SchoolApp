83ff4f566f2ff12fd19f1ca2690bb49bc05299bb
Your Name <dimmiripawan@gmail.com>
2026-03-26 14:54:41 +0530

    Frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// export const routes: Routes = [];
+
+import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Dashboard } from './dashboard/dashboard';
+export const routes: Routes = [
+  { path: '', component: Login },
+  { path: 'register', component: Register },
+  { path: 'dashboard', component: Dashboard }
+];
